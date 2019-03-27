8c196130bd810a4954344ab087f3734d80c4fad8
zawadka <louis.78100@hotmail.fr>
2019-03-27 11:14:22 +0100

    function get url

--- index.php
+++ index.php
@@ -5,3 +5,15 @@
  * Date: 26/03/2019
  * Time: 15:42
  */
+$pdo = new PDO(
+    'mysql: host=localhost; dbname=proxy;',
+    'root',
+    '',
+    [
+        // le tableau est en option ça renvoit une erreur is qq chose se passe pas bien
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
+        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+    ]
+);
+$test = $_SERVER['QUERY_STRING'];
+echo "<img src='$test' ></img>";
