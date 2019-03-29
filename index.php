e9727072ac52c80d04cb2b991a640440627a481a
Julian Romana <you@example.com>
2019-03-29 12:23:55 +0100

    To know what device you are on

--- index.php
+++ index.php
@@ -16,5 +16,24 @@ $pdo = new PDO(
       PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
   ]
 );
-$test = $_SERVER['QUERY_STRING'];
-echo "<img src='$test' ></img>";
+$test = $_SERVER['REQUEST_URI'];
+$try = explode('index.php/', $test);
+echo "<img src='$try[1]' ></img>";
+
+require_once "Mobile_Detect.php";
+
+$detect = new Mobile_Detect;
+ 
+// Any mobile device (phones or tablets).
+if ( $detect->isMobile() ) {
+  echo 'mobile';
+}
+ 
+// Any tablet device.
+else if( $detect->isTablet() ){
+  echo 'tablette';
+}else{
+  echo 'desktop';
+}
+// Check for a specific platform with the help of the magic methods:
+
